2940ace01a594434e4f31718dc98884b12745564
zed <jahkushkin@gmail.com>
2019-09-09 20:57:55 +0300

    assignment3-task2

--- HW4.1/HW4.1/ViewController.swift
+++ HW4.1/HW4.1/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         //Task 1
-        countNameSymbols()
+//        countNameSymbols()
+        //Task 2
+        checkSurnameSuffix()
     }
     //    Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
     func countNameSymbols() {
@@ -23,6 +25,23 @@ class ViewController: UIViewController {
         print("Name contain \(myName.count) symbols")
     }
 
+    //    Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
+    //    P.S. в классе написан метод который позволяет проверить на суффикс или префикс, найдите и используйте его.
+    func checkSurnameSuffix() {
+        let mySurname = "Alexandrovich"
+        //version 1
+        if mySurname.suffix(3) == "ich" {
+            print("Suffix is \"ich\"")
+        } else if mySurname.suffix(3) == "na" {
+            print("Suffix is \"na\"")
+        }
+        //version 2
+        if mySurname.hasSuffix("ich") {
+            print("Suffix is \"ich\"")
+        } else if mySurname.hasSuffix("na") {
+            print("Suffix is \"na\"")
+        }
+    }
 
 }
 
