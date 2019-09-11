d824bfb26402057c66fc7815a87dc97b93504f30
zed <jahkushkin@gmail.com>
2019-09-11 23:55:34 +0300

    assignment3-task3

--- HW4.1/HW4.1/ViewController.swift
+++ HW4.1/HW4.1/ViewController.swift
@@ -15,33 +15,59 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         //Task 1
-//        countNameSymbols()
+//        countNameSymbols("Ivanblablavan")
         //Task 2
-        checkSurnameSuffix()
+//        checkSurnameSuffix("Vasilevich")
+        //Task 3
+        divideMergedName("IvanVasilevich")
+
+        
+        
     }
     //    Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
-    func countNameSymbols() {
-        let myName = "Max"
+    func countNameSymbols(_ myName: String) {
         print("Name contain \(myName.count) symbols")
     }
 
     //    Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
     //    P.S. в классе написан метод который позволяет проверить на суффикс или префикс, найдите и используйте его.
-    func checkSurnameSuffix() {
-        let mySurname = "Alexandrovich"
+    func checkSurnameSuffix(_ myPatronymicName: String) {
         //version 1
-        if mySurname.suffix(3) == "ich" {
+        if myPatronymicName.suffix(3) == "ich" {
             print("Suffix is \"ich\"")
-        } else if mySurname.suffix(3) == "na" {
+        } else if myPatronymicName.suffix(3) == "na" {
             print("Suffix is \"na\"")
         }
         //version 2
-        if mySurname.hasSuffix("ich") {
+        if myPatronymicName.hasSuffix("ich") {
             print("Suffix is \"ich\"")
-        } else if mySurname.hasSuffix("na") {
+        } else if myPatronymicName.hasSuffix("na") {
             print("Suffix is \"na\"")
         }
     }
+    //    Задача 3. Cоздать строку, где слитно написано Ваши ИмяФамилия “IvanVasilevich"
+    //    Вам нужно разбить на две отдельных строки из предыдущей создать третью, где они обе будут разделены пробелом
+    //    str1 = “Ivan”
+    //    str2 = “Vasilevich”
+    //    str3 = “Ivan Vasilevich"
+    func divideMergedName(_ myName: String) {
+        var name = ""
+        var surname = ""
+        var uppercaseSymbolQty = 0
+        for character in myName {
+            if character.isUppercase {
+                uppercaseSymbolQty += 1
+                if uppercaseSymbolQty == 2 {
+                    name = surname
+                    surname = ""
+                    uppercaseSymbolQty = 0
+                }
+            }
+            surname.append(character)
+        }
+        print("\(name)\n\(surname)\n\(name) \(surname)")
+    }
 
+   
 }
 
